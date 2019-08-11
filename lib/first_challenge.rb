242495f23d597f45ac3dff676dc5ca5a3ce7b2ee
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-11 15:34:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,8 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
      },
      :capulet => {
-       :patriarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => []
      }
    }
 end
